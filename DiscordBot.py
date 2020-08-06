da404e6140c4ef75cf9c3a7a750e9ba73d1ec7ba
Glen <glen@simplyepic.co.uk>
2020-08-06 10:11:53 +0100

    [Bot] Cleanup imports, properly start bot and load extensions.

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,17 +1,12 @@
 import discord
-import asyncio
-import json
-import ctx as ctx
-from discord.ext import commands
-from discord.ext.commands import bot
+from discord.ext.commands import Bot
 
 
-class MyClient(discord.Client):
+class MyClient(Bot):
     #Log in
     async def on_ready(self):
         await client.change_presence(status=discord.Status.idle, activity=discord.Game("Version 0.0.3"))
         print("I am ready senpai")
-
 
     async def on_typing(self, channel , user, when):
         print(str(user) + " is typing in: " + str(channel) + str(when))
@@ -28,17 +23,7 @@ class MyClient(discord.Client):
             return
 
 
-bot = commands.Bot(command_prefix=".")
-
-#bot.load_extension(f'cogs.Commands')
-
-initial_extensions = ['cogs.Commands', 
-                      'cogs.Levels']
-
 if __name__ == '__main__':
-    for extension in initial_extensions:
-        client.load_extension(extension)
-        print(f'Loaded "{extension}"')
-
-client = MyClient()
-client.run("")
+    client = MyClient(command_prefix=".")
+    client.load_extension("Commands")
+    client.run("")

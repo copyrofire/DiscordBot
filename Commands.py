1cfb25299e12c6b5dffdfb7ae48484c8b3124d7d
Glen <glen@simplyepic.co.uk>
2020-08-06 10:11:22 +0100

    [Commands] Remove unnecessary imports, cleanup class. Bug fix ping command.

--- Commands.py
+++ Commands.py
@@ -1,16 +1,14 @@
-import commands
 from discord.ext import commands
-import ctx as ctx
     
-    
-#@commands.Cog.listener()
-class Commands(commands.Cog, name='Commands'):
+
+class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send("pong! \n{round(self.client.latency * 1000)}ms")
+        await ctx.send(f"pong! \n{round(self.bot.latency * 1000)}ms")
+
 
 def setup(bot):
     bot.add_cog(Commands(bot))

cbc9ca26851f80e0ee7041ef0776020d8da12ca6
copyrofire <69248937+copyrofire@users.noreply.github.com>
2020-09-03 14:06:15 +0200

    Seperated a command

--- Commands.py
+++ Commands.py
@@ -67,53 +67,6 @@ class Commands(commands.Cog):
             Member = ctx.message.author
 
         await ctx.send('{0} created the discord account at {0.created_at}'.format(Member))
-        
-    
-    @commands.command(pass_context = True)
-    @commands.has_permissions(kick_members=True)
-    async def warn(self, ctx, user : discord.Member, *, reason:str):
-        with open('reports.json', encoding='utf-8') as f:
-            try:
-                report = json.load(f)
-            except ValueError:
-                report = {}
-                report['users'] = []
-        if not reason:
-            await ctx.send("Please provide a reason")
-        #return #below code won't be executed
-        reason = ' '.join(reason)
-        for current_user in report['users']:
-            if current_user['id'] == user.id:
-                current_user['reasons'].append(reason)
-            break
-        else:
-            report['users'].append({
-            'id': user.id,
-            'reasons': [reason,]
-            })
-        with open('reports.json','w+') as f:
-            json.dump(report,f)
-            await ctx.send(f"{user.name} has been successfully reported!")
-
-    @commands.command(pass_context = True)
-    async def warnings(self, ctx,user:discord.User):
-        with open('reports.json', encoding='utf-8') as f:
-            try:
-                report = json.load(f)
-            except ValueError:
-                report = {}
-                report['users'] = []
-        for current_user in report['users']:
-            if user.id == current_user['id']:
-                await ctx.send(f"{user.name} has been reported {len(current_user['reasons'])} times : {','.join(current_user['reasons'])}")
-            break
-        else:
-            await ctx.send(f"{user.name} has never been reported")  
-
-    @warn.error
-    async def warn_error(self, error, ctx):
-        if isinstance(error, commands.MissingRole):
-            await ctx.send("Okay +1 warning for you, for trying to use a command u are not allowed to use!") 
 
 
     @commands.command(pass_context=True)
